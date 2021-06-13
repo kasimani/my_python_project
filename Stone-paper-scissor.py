925fce0317e49e98354f2f8b5734e2cafbe81361
msing045 <delhi.manish@gmail.com>
2021-06-13 18:06:41 +0530

    Trying-basic-game

--- Stone-paper-scissor.py
+++ Stone-paper-scissor.py
@@ -7,33 +7,33 @@ def suffle_list(computeroption):
 
 
 def player_userchoice():
-
     userchoice = ''
-    while userchoice not in ['Stone','Paper','Scissor']:
+    while userchoice not in ['stone','paper','scissor']:
         userchoice = input("Enter either Stone or Paper or Scissor:\n")
 
     return userchoice
 
 
-
-def decission(compterchoice,userchoice):
+def decission(userchoice,compterchoice):
     print ("User-Choice = " + userchoice + " Computer-choice = " + compterchoice)
-    if compterchoice == "paper" and userchoice == "Stone":
-        print("Computer Wins !!")
-    elif compterchoice == "scissor" and userchoice == "Paper":
-        print("Computer Wins !!")
-    elif compterchoice == "stone" and userchoice == "Scissor":
-        print("Computer Wins !!")
-    elif (compterchoice == "stone" and userchoice == "stone") or (compterchoice == "Paper" and userchoice == "Paper") or compterchoice == "Scissor" and userchoice == "Scissor":
-        print("Draw")
+    if compterchoice == "paper" and userchoice == "stone":
+        print("Computer Wins !!\n")
+    elif compterchoice == "scissor" and userchoice == "paper":
+        print("Computer Wins !!\n")
+    elif compterchoice == "stone" and userchoice == "scissor":
+        print("Computer Wins !!\n")
+    elif (compterchoice == "stone" and userchoice == "stone") or (compterchoice == "paper" and userchoice == "paper") or (compterchoice == "scissor" and userchoice == "scissor"):
+        print("Draw\n")
     else:
-        print("You Win")
+        print("You Win\n")
 
 
-
-computeroption = ['stone','paper','scissor']
-compterchoice = suffle_list(computeroption)
-#compterchoicerandom = random.choice(computeroption)
-#print(compterchoicerandom)
-userchoice = player_userchoice()
-decission(compterchoice,userchoice)
+while True:
+    computeroption = ['stone','paper','scissor']
+    compterchoice = random.choice(computeroption)
+    userchoice = player_userchoice()
+    print(f"Human {compterchoice}")
+    decission(userchoice,compterchoice)
+    play_again = input("Want to play again? (Y/N)")
+    if play_again.lower() != "y":
+        break
