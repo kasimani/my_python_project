fac2326bb623445d3bf9aacf56ad6effe77ff852
msing045 <delhi.manish@gmail.com>
2021-06-13 17:19:03 +0530

    Trying-basic-game

--- Stone-paper-scissor.py
+++ Stone-paper-scissor.py
@@ -1,1 +1,39 @@
+import random
 from random import shuffle
+
+def suffle_list(computeroption):
+    shuffle(computeroption)
+    return computeroption[0]
+
+
+def player_userchoice():
+
+    userchoice = ''
+    while userchoice not in ['Stone','Paper','Scissor']:
+        userchoice = userinput = input("Enter either Stone or Paper or Scissor:\n")
+
+    return userchoice
+
+
+
+def decission(compterchoice,userchoice):
+    print ("User-Choice = " + userchoice + " Computer-choice = " + compterchoice)
+    if compterchoice == "paper" and userchoice == "Stone":
+        print("Computer Wins !!")
+    elif compterchoice == "scissor" and userchoice == "Paper":
+        print("Computer Wins !!")
+    elif compterchoice == "stone" and userchoice == "Scissor":
+        print("Computer Wins !!")
+    elif (compterchoice == "stone" and userchoice == "stone") or (compterchoice == "Paper" and userchoice == "Paper") or compterchoice == "Scissor" and userchoice == "Scissor":
+        print("Draw")
+    else:
+        print("You Win")
+
+
+
+computeroption = ['stone','paper','scissor']
+compterchoice = suffle_list(computeroption)
+#compterchoicerandom = random.choice(computeroption)
+#print(compterchoicerandom)
+userchoice = player_userchoice()
+decission(compterchoice,userchoice)
